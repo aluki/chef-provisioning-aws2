a3f5fdc9f7c09b0f928b77e6f79577018b0e1149
Alberto Tablado <alberto@aluki.org>
2015-03-17 18:42:58 +0000

    Remove unused code

--- lib/chef/provisioning/aws2.rb
+++ lib/chef/provisioning/aws2.rb
@@ -1,10 +1,3 @@
 require "chef/provisioning/aws2/version"
 require "chef/provisioning/aws2/driver"
 
-module Chef
-  module Provisioning
-    module Aws2
-      # Your code goes here...
-    end
-  end
-end
